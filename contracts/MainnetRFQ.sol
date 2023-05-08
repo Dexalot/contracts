fe3882cf711f33913c30970ef0dc2efea1d8c74b
MarJon_Call <markjonathas@gmail.com>
2023-05-08 14:00:10 -0600

    Added ability to update quote expiry & makerAmount

--- contracts/MainnetRFQ.sol
+++ contracts/MainnetRFQ.sol
@@ -12,16 +12,23 @@ import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeab
 
 /**
  * @title   Request For Quote smart contract
- * @notice  This contract takes advantages of prices from the dexalot subnet to provide
+ * @notice  This contract takes advantage of prices from the Dexalot subnet to provide
  * token swaps on C-Chain. Currently, users must perform a simple swap via our RFQ API.
  * @dev After getting a firm quote from our off chain RFQ API, call the simpleSwap() function with
  * the quote. This will execute a swap, exchanging the taker asset (asset you provide) with
- * the maker asset (asset we provide).
+ * the maker asset (asset we provide). In times of high volatility, the API may adjust your quoted
+ * price. The price will never be lower than slippageTolerance, which represents a percentage of the 
+ * original quoted price. To check if your quoted price has been affected by slippage, monitor the SlippageApplied
+ * event. The expiry of your quote may also be adjusted during times of high volatility. Monitor the ExpiryUpdated
+ * event to verify if the deadline has been updated. It is highly unlikely that your quotes's makerAmount and expiry
+ * are updated. Adjusting the quote is rare, and only resorted to in periods of high volatility for quotes that do 
+ * not properly represent the liquidity of the Dexalot subnet.
  */
 
 // The code in this file is part of Dexalot project.
 // Please see the LICENSE.txt file for licensing info.
-// Copyright 2022 Dexalot.
+// Copyright 2023 Dexalot.
+
 contract MainnetRFQ is
     AccessControlEnumerableUpgradeable,
     PausableUpgradeable,
@@ -34,12 +41,21 @@ contract MainnetRFQ is
     // version
     bytes32 public constant VERSION = bytes32("1.0.0");
 
+    // rebalancer admin role
+    bytes32 public constant REBALANCER_ADMIN_ROLE = keccak256("REBALANCER_ADMIN_ROLE");
+
     // address used to sign transactions from Paraswap API
     address public swapSigner;
-    address public rebalancer;
 
-    // keeps track of trade nonces
+    // max slippage tolerance for updated quote in BIPs
+    uint256 public slippageTolerance;
+
+    // keeps track of trade nonces executed
     mapping(uint256 => bool) private nonceUsed;
+    // keeps track of trade nonces that had an updated expiry
+    mapping(uint256 => uint256) public quoteMakerAmountUpdated;
+    // keeps track of trade nonces that had slippage applied to their quoted price
+    mapping(uint256 => uint256) public quoteExpiryUpdated;
 
     // whitelisted smart contracts. Only applicable if msg.sender is not _quote.taker
     mapping(address => bool) public trustedContracts;
@@ -47,11 +63,14 @@ contract MainnetRFQ is
     // contract address to integrator organization name
     mapping(address => string) public trustedContractToIntegrator;
 
-    event SwapSignerUpdated(address _newSwapSigner);
-    event RebalancerUpdated(address _rebalancer);
+    // storage gap for upgradeability
+    uint256[50] __gap; 
+
+    event SwapSignerUpdated(address newSwapSigner);
     event RoleUpdated(string indexed name, string actionName, bytes32 updatedRole, address updatedAddress);
     event AddressSet(string indexed name, string actionName, address newAddress);
     event SwapExecuted(
+        uint256 nonceAndMeta,
         address maker,
         address taker,
         address makerAsset,
@@ -59,7 +78,10 @@ contract MainnetRFQ is
         uint256 makerAmountReceived,
         uint256 takerAmountReceived
     );
-    event RebalancerWithdraw(address _asset, uint256 _amount);
+    event RebalancerWithdraw(address asset, uint256 amount);
+    event SlippageApplied(uint256 nonceAndMeta, uint256 newMakerAmount);
+    event ExpiryUpdated(uint256 nonceAndMeta, uint256 newExpiry);
+    event SlippageToleranceUpdated(uint256 newSlippageTolerance);
 
     // firm quote data structure sent to user from Paraswap API
     struct Quote {
@@ -73,33 +95,38 @@ contract MainnetRFQ is
         uint256 takerAmount;
     }
 
+
     /**
      * @notice  initializer function for Upgradeable RFQ
+     * @dev slippageTolerance is initially set to 9700. slippageTolerance is represented in BIPs, 
+     * therefore slippageTolerance is effectively set to 97%. This means that the price of a firm quote
+     * can not drop more than 3% initially.
      * @param _swapSigner Address of swap signer, rebalancer is also defaulted to swap signer
      * but it can be changed later
      */
-    function initialize(address _swapSigner) public initializer {
+    function initialize(address _swapSigner) external initializer {
+        require(_swapSigner != address(0), "RF-SAZ-01");
         __AccessControlEnumerable_init();
         __Pausable_init();
         __ReentrancyGuard_init();
         __EIP712_init("Dexalot", "1.0.0");
 
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(REBALANCER_ADMIN_ROLE, _swapSigner);
 
         swapSigner = _swapSigner;
-        rebalancer = _swapSigner;
+        slippageTolerance = 9700;
     }
 
     /**
-     * @notice Swaps two ERC-20 tokens.
-     * @dev This function can only be called after generating a firm quote from the Paraswap API.
-     * All parameters are generated from the Paraswap API.
-     * @param _quote Trade parameters for swap generated from /api/ps/firm
-     * @param _signature Signature of trade parameters generated from /api/ps/firm
+     * @notice Swaps two Assets, based off a predetermined swap price.
+     * @dev This function can only be called after generating a firm quote from the RFQ API.
+     * All parameters are generated from the RFQ API. Prices are determined based off of trade
+     * prices from the Dexalot subnet.
+     * @param _quote Trade parameters for swap generated from /api/rfq/firm
+     * @param _signature Signature of trade parameters generated from /api/rfq/firm
      **/
-
     function simpleSwap(Quote calldata _quote, bytes calldata _signature) external payable whenNotPaused nonReentrant {
-        require(block.timestamp <= _quote.expiry, "RF-QE-01");
         require(nonceUsed[_quote.nonceAndMeta] == false, "RF-IN-01");
         require(_quote.taker == msg.sender || trustedContracts[msg.sender], "RF-IMS-01");
 
@@ -109,6 +136,7 @@ contract MainnetRFQ is
         bytes32 structType = keccak256(
             "Quote(uint256 nonceAndMeta,uint256 expiry,address makerAsset,address takerAsset,address maker,address taker,uint256 makerAmount,uint256 takerAmount)"
         );
+
         bytes32 hashedStruct = keccak256(
             abi.encode(
                 structType,
@@ -126,30 +154,88 @@ contract MainnetRFQ is
         address messageSigner = digest.recover(_signature);
         require(messageSigner == swapSigner, "RF-IS-01");
 
+        // verifies if quote expiry updated by checking in mapping
+        // if the expiry is less than the current timestamp, then 
+        // the transaction reverts 
+        if(quoteExpiryUpdated[_quote.nonceAndMeta] != 0) {
+            require(block.timestamp <= quoteExpiryUpdated[_quote.nonceAndMeta], "RF-QE-01");
+        } else {
+            require(block.timestamp <= _quote.expiry, "RF-QE-01");
+        }
+
+        // verifies if slippage was applied to the quoted makerAmount
+        // by checking in the mapping. If not, the original quoted price
+        // is used for the trade
+        uint256 makerAmount = quoteMakerAmountUpdated[_quote.nonceAndMeta];
+        if(makerAmount == 0) {
+            makerAmount = _quote.makerAmount;
+        } 
+        
         if (_quote.makerAsset == address(0)) {
             // swap NATIVE <=> ERC-20
             IERC20Upgradeable(_quote.takerAsset).safeTransferFrom(_quote.taker, address(this), _quote.takerAmount);
             // solhint-disable-next-line avoid-low-level-calls
-            (bool success, ) = payable(_quote.taker).call{value: _quote.makerAmount}("");
-            require(success == true, "RF-TF-01");
+            (bool success, ) = payable(_quote.taker).call{value: makerAmount}("");
+            require(success, "RF-TF-01");
         } else if (_quote.takerAsset == address(0)) {
             // swap ERC-20 <=> NATIVE
             require(msg.value == _quote.takerAmount, "RF-IMV-01");
-            IERC20Upgradeable(_quote.makerAsset).safeTransfer(_quote.taker, _quote.makerAmount);
+            IERC20Upgradeable(_quote.makerAsset).safeTransfer(_quote.taker, makerAmount);
         } else {
             // swap ERC-20 <=> ERC-20
             IERC20Upgradeable(_quote.takerAsset).safeTransferFrom(_quote.taker, address(this), _quote.takerAmount);
-            IERC20Upgradeable(_quote.makerAsset).safeTransfer(_quote.taker, _quote.makerAmount);
+            IERC20Upgradeable(_quote.makerAsset).safeTransfer(_quote.taker, makerAmount);
         }
-
+        
         emit SwapExecuted(
+            _quote.nonceAndMeta,
             _quote.maker,
             _quote.taker,
             _quote.makerAsset,
             _quote.takerAsset,
-            _quote.makerAmount,
+            makerAmount,
             _quote.takerAmount
         );
+    }
+
+    /**
+     * @notice Updates the expiry of a quote. The new expiry
+     * is the deadline a trader has to execute the swap.
+     * @dev Only rebalancer can call this function.
+     * @param _nonceAndMeta nonce of quote
+     * @param _newExpiry new expiry for quote
+     **/
+    function updateQuoteExpiry(uint256 _nonceAndMeta, uint256 _newExpiry) external onlyRole(REBALANCER_ADMIN_ROLE) {
+        quoteExpiryUpdated[_nonceAndMeta] = _newExpiry;
+        emit ExpiryUpdated(_nonceAndMeta, _newExpiry);
+    }
+
+    /**
+     * @notice Updates the makerAmount of a quote.
+     * The new makerAmount can not be lower than the percentage 
+     * of slippageTolerance from the previous quoted price.
+     * @dev Only rebalancer can call this function.
+     * @param _nonceAndMeta nonce of quote
+     * @param _newMakerAmount new makerAmount for quote
+     **/
+    function updateQuoteMakerAmount(uint256 _nonceAndMeta, uint256 _newMakerAmount, uint256 _oldMakerAmount) external onlyRole(REBALANCER_ADMIN_ROLE) {
+        uint256 lowestAllowedPriceAfterSlippage = _oldMakerAmount * slippageTolerance / 10000;
+        require(lowestAllowedPriceAfterSlippage < _newMakerAmount, "RF-TMS"); 
+        quoteMakerAmountUpdated[_nonceAndMeta] = _newMakerAmount;
+        emit SlippageApplied(_nonceAndMeta, _newMakerAmount);
+    }
+
+    /**
+     * @notice Updates the slippageTolerance for a quote update. 
+     * i.e. slippageTolerance = 9700 (97%), _oldMakerAmount = 100
+     * _newMakerAmount must be greater than if not equal to 97
+     * 97 = 100 * 9700 / 10000
+     * @dev Only default admin can call this function.
+     * @param _newSlippageTolerance lowest percent of original makerAmount allowed in BIPs
+     **/
+    function setSlippageTolerance(uint256 _newSlippageTolerance) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        slippageTolerance = _newSlippageTolerance;
+        emit SlippageToleranceUpdated(_newSlippageTolerance);
     }
 
     /**
@@ -164,14 +250,31 @@ contract MainnetRFQ is
     }
 
     /**
-     * @notice Updates the rebalancer address.
-     * @dev Only DEFAULT_ADMIN can call this function.
-     * @param _rebalancer Address of new rebalancer
-     **/
-    function setRebalancer(address _rebalancer) external onlyRole(DEFAULT_ADMIN_ROLE) {
-        require(_rebalancer != address(0), "RF-SAZ-01");
-        rebalancer = _rebalancer;
-        emit RebalancerUpdated(_rebalancer);
+     * @notice  Adds Rebalancer Admin role to the address
+     * @param   _address  address to add role to
+     */
+    function addRebalancer(address _address) external virtual onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(_address != address(0), "RF-SAZ-01");
+        emit RoleUpdated("RFQ", "ADD-ROLE", REBALANCER_ADMIN_ROLE, _address);
+        grantRole(REBALANCER_ADMIN_ROLE, _address);
+    }
+
+    /**
+     * @notice  Removes Rebalancer Admin role from the address
+     * @param   _address  address to remove role from
+     */
+    function removeRebalancer(address _address) external virtual onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(getRoleMemberCount(REBALANCER_ADMIN_ROLE) > 1, "RF-ALOA-01");
+        emit RoleUpdated("RFQ", "REMOVE-ROLE", REBALANCER_ADMIN_ROLE, _address);
+        revokeRole(REBALANCER_ADMIN_ROLE, _address);
+    }
+
+    /**
+     * @param   _address  address to check
+     * @return  bool    true if address has Rebalancer Admin role
+     */
+    function isRebalancer(address _address) external view returns (bool) {
+        return hasRole(REBALANCER_ADMIN_ROLE, _address);
     }
 
     /**
@@ -179,6 +282,7 @@ contract MainnetRFQ is
      * @param   _address  address to add role to
      */
     function addAdmin(address _address) external virtual onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(_address != address(0), "RF-SAZ-01");
         emit RoleUpdated("RFQ", "ADD-ROLE", DEFAULT_ADMIN_ROLE, _address);
         grantRole(DEFAULT_ADMIN_ROLE, _address);
     }
@@ -211,6 +315,7 @@ contract MainnetRFQ is
         address _contract,
         string calldata _organization
     ) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(_contract != address(0), "RF-SAZ-01");
         trustedContracts[_contract] = true;
         trustedContractToIntegrator[_contract] = _organization;
         emit AddressSet(_organization, "RF-ADD-TRUSTEDCONTRACT", _contract);
@@ -256,16 +361,13 @@ contract MainnetRFQ is
      * @param   _asset  Address of the asset to be withdrawn
      * @param   _amount  Amount of asset to be withdrawn
      */
-    function claimBalance(address _asset, uint256 _amount) external nonReentrant {
-        address _rebalancer = rebalancer;
-        require(msg.sender == _rebalancer, "RF-OCR-01");
-
+    function claimBalance(address _asset, uint256 _amount) external onlyRole(REBALANCER_ADMIN_ROLE) nonReentrant {
         if (_asset == address(0)) {
             // solhint-disable-next-line avoid-low-level-calls
-            (bool success, ) = payable(_rebalancer).call{value: _amount}("");
-            require(success == true, "RF-TF-01");
+            (bool success, ) = payable(msg.sender).call{value: _amount}("");
+            require(success, "RF-TF-01");
         } else {
-            IERC20Upgradeable(_asset).safeTransfer(_rebalancer, _amount);
+            IERC20Upgradeable(_asset).safeTransfer(msg.sender, _amount);
         }
         emit RebalancerWithdraw(_asset, _amount);
     }
@@ -273,20 +375,19 @@ contract MainnetRFQ is
     /**
      * @notice  Allows rebalancer to withdraw multiple assets from smart contract
      * @dev     Only callable by admin
-     * @param   _assets  Array of addresses of the asset to be withdrawn
-     * @param   _amounts  Array of Amount of assets to be withdrawn
+     * @param   _assets  Array of addresses of the assets to be withdrawn
+     * @param   _amounts  Array of amounts of assets to be withdrawn
      */
-    function batchClaimBalance(address[] calldata _assets, uint256[] calldata _amounts) external nonReentrant {
-        require(msg.sender == rebalancer, "RF-OCR-01");
+    function batchClaimBalance(address[] calldata _assets, uint256[] calldata _amounts) external onlyRole(REBALANCER_ADMIN_ROLE) nonReentrant {
         uint256 i;
 
         while (i < _assets.length) {
             if (_assets[i] == address(0)) {
                 // solhint-disable-next-line avoid-low-level-calls
-                (bool success, ) = payable(rebalancer).call{value: _amounts[i]}("");
-                require(success == true, "RF-TF-01");
+                (bool success, ) = payable(msg.sender).call{value: _amounts[i]}("");
+                require(success, "RF-TF-01");
             } else {
-                IERC20Upgradeable(_assets[i]).safeTransfer(rebalancer, _amounts[i]);
+                IERC20Upgradeable(_assets[i]).safeTransfer(msg.sender, _amounts[i]);
             }
             emit RebalancerWithdraw(_assets[i], _amounts[i]);
             unchecked {
@@ -298,7 +399,5 @@ contract MainnetRFQ is
     /**
      * @dev  Used to rebalance rfq contract
      */
-    receive() external payable {
-        require(msg.sender == rebalancer, "RF-OCR-01");
-    }
+    receive() external payable onlyRole(REBALANCER_ADMIN_ROLE) { }
 }
