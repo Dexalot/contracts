fb5b13ad468d0ac3c14d89428919abe99ed84a15
Jaimi Patel <jaimip34@gmail.com>
2026-02-24 01:37:51 +0000

    F-2026-14845: correctly document ContractAccess type

--- contracts/interfaces/IOmniVaultExecutor.sol
+++ contracts/interfaces/IOmniVaultExecutor.sol
@@ -3,10 +3,10 @@ pragma solidity 0.8.30;
 
 interface IOmniVaultExecutor {
     enum ContractAccess {
-        NONE,
-        TRUSTED,
-        NATIVE,
-        ERC20,
-        NATIVE_AND_ERC20
+        NONE, // Not trusted, cannot call any functions or send/receive funds
+        TRUSTED, // Trusted contract, can call whitelisted functions that don't send/receive funds
+        NATIVE, // Trusted contract, can call whitelisted functions that send/receive native currency
+        ERC20, // Trusted contract, can call whitelisted functions that send/receive ERC20 tokens
+        NATIVE_AND_ERC20 // Trusted contract, can call whitelisted functions that send/receive both native currency and ERC20 tokens
     }
 }
